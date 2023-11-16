3362ecf9e782535c277b29cd59d665977d8b4a59
tudark179@gmail.com <87460513+tudark179@users.noreply.github.com>
2023-11-17 00:01:04 +0700

    update vpc.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -3,6 +3,14 @@ resource "aws_security_group" "backstage_sg" {
   description = "Security group for Backstage instance"
   vpc_id      = aws_vpc.backstage_vpc.id
 }
+resource "aws_security_group_rule" "allow_http" {
+  type        = "ingress"
+  from_port   = 3000
+  to_port     = 3000
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.backstage_sg.id
+}
 
 resource "aws_iam_role" "backstage_role" {
   name = "backstage_role"
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,11 +5,33 @@ resource "aws_vpc" "backstage_vpc" {
   }
 }
 
+resource "aws_internet_gateway" "backstage_igw" {
+  vpc_id = aws_vpc.backstage_vpc.id
+  tags = {
+    Name = "BackstageIGW"
+  }
+}
+
+resource "aws_route_table" "backstage_rt" {
+  vpc_id = aws_vpc.backstage_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.backstage_igw.id
+  }
+  tags = {
+    Name = "BackstageRouteTable"
+  }
+}
+
 resource "aws_subnet" "backstage" {
   vpc_id     = aws_vpc.backstage_vpc.id
   cidr_block = "10.0.1.0/24"
-
   tags = {
     Name = "BackstageSubnet"
   }
+}
+
+resource "aws_route_table_association" "backstage_rta" {
+  subnet_id      = aws_subnet.backstage.id
+  route_table_id = aws_route_table.backstage_rt.id
 }
